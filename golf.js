1fbb47ab84de7245a0a3938871a3914a7ee2a3a9
Summer Brown <slbrown07@gmail.com>
2019-11-19 20:39:50 -0700

    added hcp and par to score card

--- golf.js
+++ golf.js
@@ -27,7 +27,7 @@ function getCourses() {
                     `<figure>
                         <img src="${allCourses.courses[i].image}">
                         <figcaption>${allCourses.courses[i].name}</figcaption>
-                        <button onclick="golfLevels(${allCourses.courses[i].id}, 0)">Select</button>
+                        <button onclick="buildCard(${allCourses.courses[i].id})">Select</button>
                     </figure>`;
             }
         }
@@ -37,17 +37,29 @@ function getCourses() {
     xhttp.send();
 }
 
-function golfLevels(golfId, levelId) {
+function golfLevels(golfId) {
     let xhttp = new XMLHttpRequest(); // new ajax request
     xhttp.onreadystatechange = function() {
         if(this.readyState == 4 && this.status == 200) {
             myLevels = JSON.parse(this.responseText);
-            let levelType = `${myLevels.data.holes[0].teeBoxes[levelId].teeType}`;
-            document.getElementById('proLevel').innerHTML = 
-                `<div class="${levelType}">${levelType}</div>`;
-            for (let i=0; i<myLevels.data.holes.length; i++) {
-                document.getElementById('proLevel').innerHTML +=
-                    `<div class="pro">${myLevels.data.holes[i].teeBoxes[levelId].yards}</div>`;
+            for (let x=0; x<4; x++) {
+                let levelType = `${myLevels.data.holes[0].teeBoxes[x].teeType}`;
+                let parType = `${myLevels.data.holes[0].teeBoxes[x].par}`;
+                let hcpType = `${myLevels.data.holes[0].teeBoxes[x].hcp}`;
+                document.getElementById(`${levelType}`).innerHTML = 
+                    `<div class="${levelType}">${levelType.toUpperCase()}</div>`;
+                document.getElementById('par').innerHTML = 
+                    `<div class="par">PAR</div>`;
+                document.getElementById('hcp').innerHTML = 
+                    `<div class="hcp">HANDICAP</div>`;
+                for (let i=0; i<myLevels.data.holes.length; i++) {
+                    document.getElementById(`${levelType}`).innerHTML +=
+                        `<div class="${levelType}">${myLevels.data.holes[i].teeBoxes[x].yards}</div>`;
+                    document.getElementById('par').innerHTML +=
+                        `<div class="par">${myLevels.data.holes[i].teeBoxes[x].par}</div>`;
+                    document.getElementById('hcp').innerHTML +=
+                        `<div class="hcp">${myLevels.data.holes[i].teeBoxes[x].hcp}</div>`;
+                }
             }
         }                            
     };
@@ -55,14 +67,14 @@ function golfLevels(golfId, levelId) {
     xhttp.send();
 }
 
-function scoreCard(golfId) {
+function buildCard(golfId) {
 
     let xhttp = new XMLHttpRequest(); // new ajax request
     xhttp.onreadystatechange = function() {
         if(this.readyState == 4 && this.status == 200) {
             myCourse = JSON.parse(this.responseText);
 
-            // header information for score card
+            // Selected Golf Course information at Top of Score Card
 
             document.getElementById('courseName').innerHTML = myCourse.data.name;
             document.getElementById('courseAddress').innerHTML = myCourse.data.addr1;
@@ -70,67 +82,58 @@ function scoreCard(golfId) {
                 `${myCourse.data.city}, ${myCourse.data.stateOrProvince}  ${myCourse.data.zipCode}`;
             document.getElementById('coursePhone').innerHTML = 
                 `${myCourse.data.phone} <i class="fas fa-golf-ball"></i> ${myCourse.data.website}`;
-            
-            // option to select pro, champion etc
-            if (levelDisplay.pro == 'no') { 
-                document.getElementById('levelOptions').innerHTML = 
-                    `<div><i class="far fa-circle"></i> Pro</div>`;
-            } else {
-                document.getElementById('levelOptions').innerHTML = 
-                    `<div><i class="far fa-check-circle"></i> Pro</div>`;
-            }
-            if (levelDisplay.champion == 'no') { 
-                document.getElementById('levelOptions').innerHTML += 
-                    `<div><i class="far fa-circle"></i> Champion</div>`;
-            } else {
-                document.getElementById('levelOptions').innerHTML += 
-                    `<div><i class="far fa-check-circle"></i> Champion</div>`;
-            }
-            if (levelDisplay.men == 'no') { 
-                document.getElementById('levelOptions').innerHTML += 
-                    `<div><i class="far fa-circle"></i> Men</div>`;
-            } else {
-                document.getElementById('levelOptions').innerHTML += 
-                    `<div><i class="far fa-check-circle"></i> Men</div>`;
-            }
-            if (levelDisplay.women == 'no') { 
-                document.getElementById('levelOptions').innerHTML += 
-                    `<div><i class="far fa-circle"></i> Women</div>`;
-            } else {
-                document.getElementById('levelOptions').innerHTML += 
-                    `<div><i class="far fa-check-circle"></i> Women</div>`;
-            }
-        } 
-                               
+        }                          
     };
     xhttp.open('GET', `https://golf-courses-api.herokuapp.com/courses/${golfId}`, true);
     xhttp.send();
 
-    // create header row
-    document.getElementById('rowHeaders').innerHTML += `<div id='inScore'>Name</div>`;
+    // Create Column Headers
+    document.getElementById('colHead').innerHTML += `<div class="nameCol">Name</div>`;
     for(let i = 1; i <= 9; i++) {
-        document.getElementById('rowHeaders').innerHTML += `<div class="titleRow">Hole ${i}</div>`;
+        document.getElementById('colHead').innerHTML += `<div class="hole${i}">Hole ${i}</div>`;
     }
-    document.getElementById('rowHeaders').innerHTML += `<div id='inScore'>In Score</div>`;
+    document.getElementById('colHead').innerHTML += `<div class="inScore">In Score</div>`;
     for(let i=10; i<=18; i++){
-        document.getElementById('rowHeaders').innerHTML += `<div>Hole ${i}</div>`;
+        document.getElementById('colHead').innerHTML += `<div class="hole${i}>Hole ${i}</div>`;
     }
-    document.getElementById('rowHeaders').innerHTML += `<div id='outScore'>Out Score</div>`;
-    document.getElementById('rowHeaders').innerHTML += `<div id='totalScore'>Total Score</div>`;
+    document.getElementById('colHead').innerHTML += `<div class="OutScore">Out Score</div>`;
+    document.getElementById('colHead').innerHTML += `<div id="totalScore">Total Score</div>`;
 
+    // Load pro, champion, men, women par and hcp
+
+    golfLevels(golfId);
 
 }
 
 function proDisplay() {
-    let xhttp = new XMLHttpRequest(); // new ajax request
-    xhttp.onreadystatechange = function() {
-        if(this.readyState == 4 && this.status == 200) {
-            myCourse = JSON.parse(this.responseText);
-            console.log(myCourse);
-        }
-    };
-    xhttp.open('GET', `https://golf-courses-api.herokuapp.com/courses/18300`, true);
-    xhttp.send();
+    if (levelDisplay.pro == 'no') { 
+        document.getElementById('levelOptions').innerHTML = 
+            `<div><i class="far fa-circle"></i> Pro</div>`;
+    } else {
+        document.getElementById('levelOptions').innerHTML = 
+            `<div><i class="far fa-check-circle"></i> Pro</div>`;
+    }
+    if (levelDisplay.champion == 'no') { 
+        document.getElementById('levelOptions').innerHTML += 
+            `<div><i class="far fa-circle"></i> Champion</div>`;
+    } else {
+        document.getElementById('levelOptions').innerHTML += 
+            `<div><i class="far fa-check-circle"></i> Champion</div>`;
+    }
+    if (levelDisplay.men == 'no') { 
+        document.getElementById('levelOptions').innerHTML += 
+            `<div><i class="far fa-circle"></i> Men</div>`;
+    } else {
+        document.getElementById('levelOptions').innerHTML += 
+            `<div><i class="far fa-check-circle"></i> Men</div>`;
+    }
+    if (levelDisplay.women == 'no') { 
+        document.getElementById('levelOptions').innerHTML += 
+            `<div><i class="far fa-circle"></i> Women</div>`;
+    } else {
+        document.getElementById('levelOptions').innerHTML += 
+            `<div><i class="far fa-check-circle"></i> Women</div>`;
+    }
 }
 
 
